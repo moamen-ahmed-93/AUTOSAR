4f8ef94bff03066f964d42de749eb7cea62dc3ad
Islam El-Bahnasawy <islam.elbahnasawy@gmail.com>
2020-02-26 13:48:12 +0200

    Update LAMP.h

--- ECUAL/LAMP.h
+++ ECUAL/LAMP.h
@@ -14,8 +14,21 @@
 #define SALOON_PORT 	GPIO_PORTA
 #define SALOON_PIN 		GPIO_PIN0
 
+/* lamp_CH options:
+		1. SALOON_LAMP
+*/
 
 Error_Status Lamp_Init (u8 lamp_CH);
+
+
+
+/* lamp_CH options:
+		1. SALOON_LAMP
+
+	lamp_status options:
+		1. LAMP_ON 
+		2. LAMP_OFF
+*/
 Error_Status Lamp_update (u8 lamp_CH , u8 lamp_status);
 
 #endif
