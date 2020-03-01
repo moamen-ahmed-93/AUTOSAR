0d8dfbe5ad6613213893e7758af8a42d73f4b45c
AhmedQaneel9996 <59745955+AhmedQaneel9996@users.noreply.github.com>
2020-03-01 22:42:37 +0200

    Change in ECUAL (LAMP.h, DoorSensor.h)

--- ECUAL/DoorSensing_interface.h
+++ ECUAL/DoorSensing_interface.h
@@ -1,13 +1,58 @@
+
+/************************************************************************
+ * File name: DoorSensing.h
+ *
+ *
+ * Description: This file contains the prototypes of the DoorSensing driver
+ * and the macros used by the user
+ *
+ * owner: Group Autosar
+ * date: 27/2/2020
+ * version 1.1
+ *
+ ***************************************************************************/
+
+/************************************************************************
+ * Function name: DoorSensor_Init
+ *
+ * parameters:  Input:
+ *                 Door
+ *                     type: u8
+                        Description: Left OR Right Sensor
+ *              Output: NA
+ *              In/out: NA
+ * return: E_OK, E_NOK
+ * Description: a function to Initialize Sensor With GPIO Driver
+ ***************************************************************************/
 Error_Status DoorSensor_Init(u8 Door);
+
+/************************************************************************
+ * Function name: GPIO_Init
+ *
+ * parameters:  Input:
+ *                 sensor_Ch
+ *                     type: u8
+                        Description: Sensor Channels (Channel 1 , Channel 2)
+ *                 Status
+ *                     type: u8 *
+ *                     Description: Status Of the Sensor
+ *
+ *              Output: NA
+ *              In/out: NA
+ * return: E_OK, E_NOK
+ * Description: a function to read the status of the sensor according to Channel 
+ ***************************************************************************/
 Error_Status DoorSensor_ReadStatus(u8 sensor_Ch, u8* Status);
 
+
+/* DoorSensor_1 Mapping */
 #define DOORSENSOR1_PORT   GPIO_PORTA
 #define DOORSENSOR1_PIN     GPIO_PIN2
 #define DOORSENSOR1_MODE    MODE_INPUT
-
+/* DoorSensor_2 Mapping */
 #define DOORSENSOR2_PORT   GPIO_PORTA
 #define DOORSENSOR2_PIN     GPIO_PIN3
 #define DOORSENSOR2_MODE    MODE_INPUT
-
+/* Sensors Mapping */
 #define LEFTSENSOR          1
 #define RIGHTSENSOR         2
--- ECUAL/LAMP.h
+++ ECUAL/LAMP.h
@@ -3,32 +3,50 @@
 
 #define IN_MODE 		0
 #define OUT_MODE 		1
-	
+
+/*States Of The Lamp*/
 #define LAMP_ON  		HIGH
 #define LAMP_OFF 		LOW
 
-
+/*Number Of Lamps*/
 #define NUM_OF_LAMPS  	1
 
+/*Lamp Maping*/
 #define SALOON_LAMP 	0
 #define SALOON_PORT 	GPIO_PORTA
 #define SALOON_PIN 		GPIO_PIN4
 
-/* lamp_CH options:
-		1. SALOON_LAMP
-*/
-
+/************************************************************************
+ * Function name: Lamp_Init
+ *
+ * parameters:  Input:
+ *                 lamp_CH
+ *                     type: u8
+                        Description: Choose Lamp channel
+ *              Output: NA
+ *              In/out: NA
+ * return: E_OK, E_NOK
+ * Description: a function to Initialize Lamp 
+ ***************************************************************************/
 Error_Status Lamp_Init (u8 lamp_CH);
 
 
 
-/* lamp_CH options:
-		1. SALOON_LAMP
-
-	lamp_status options:
-		1. LAMP_ON 
-		2. LAMP_OFF
-*/
+/************************************************************************
+ * Function name: Lamp_update
+ *
+ * parameters:  Input:
+ *                 lamp_CH
+ *                     type: u8
+                        Description: Choose Lamp Channel
+ *                 lamp_status
+ *                     type: u8
+ *                     Description: Lamp Status (LAMP_ON,LAMP_OFF)
+ *              Output: NA
+ *              In/out: NA
+ * return: E_OK, E_NOK
+ * Description: a function to update the lamp Status
+ ***************************************************************************/
 Error_Status Lamp_update (u8 lamp_CH , u8 lamp_status);
 
 #endif
